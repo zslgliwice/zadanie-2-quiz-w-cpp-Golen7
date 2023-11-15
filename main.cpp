8d8ba407a696f82eeec9032a40e684543bc0a885
Mateusz <88245072+Golen7@users.noreply.github.com>
2023-11-15 16:54:32 +0100

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -9,47 +9,107 @@ string odp;
 
 int main()
 {
+    int i = 0;
+    cout << "Ladowanie";
+    while (i < 2) {
+        i++;
+        Sleep(1000);
+        cout << "." << flush;
+        Sleep(1000);
+        cout << "." << flush;
+        Sleep(1000);
+        cout << "." << flush;
+        Sleep(1000);
+        cout << "\b\b\b   \b\b\b" << flush;
+    }
+    system("cls");
+
     cout << "\aPodaj imie: ";
     cin >> imie;
     int pkt = 0;
 
-    int i = 0;
-    cout<<"Ladowanie";
-    while (i<2){
-        i++;
-        Sleep(1);
-        cout<<"."<<flush;
-        Sleep(1);
-        cout<<"."<<flush;
-        Sleep(1);
-        cout<<"."<<flush;
-        Sleep(1);
-        cout << "\b\b\b   \b\b\b"<<flush;
-    }
-    system("cls");
+    cout << "Witaj " << imie << " zapraszam do krotkiego testu\n";
 
-    cout << imie << "Zapraszam do krotkiego testu\n";
-
-    cout << "Co to jest C++?"<<endl;
-    cout << "a) Piosenkarka"<<endl;
-    cout << "b) Jezyk programowania"<<endl;
-    cout << "c) Mlynek do herbaty"<<endl;
+    cout << "Co to jest C++?" << endl;
+    cout << "a) Piosenkarka" << endl;
+    cout << "b) Jezyk programowania" << endl;
+    cout << "c) Mlynek do herbaty" << endl;
     cout << "Twoja odpowiedz: ";
     cin >> odp;
-    if(odp == "b")
+    if (odp == "b")
+    {
+        cout << "Poprawna odpowiedz!";
+        pkt++;
+    }else cout << "Zle! Jest to jezyk programowania!";
+
+    Sleep(3000);
+    system("cls");
+
+    cout << "Ktory program jest programem graficznym?" << endl;
+    cout << "a) Word" << endl;
+    cout << "b) Paint" << endl;
+    cout << "c) Minecraft" << endl;
+    cout << "Twoja odpowiedz: ";
+    cin >> odp;
+    if (odp == "b")
     {
         cout << "Poprawna odpowiedz!";
         pkt++;
     }
-    else
-    {
-        cout << "Zle! Jest to jezyk programowania!";
+    else cout << "Zle! Programem graficznym jest Paint";
 
-    }
     Sleep(3000);
     system("cls");
 
+    cout << "Ktore polecenie oznacza petle w jezykach programowania?" << endl;
+    cout << "a) for" << endl;
+    cout << "b) if" << endl;
+    cout << "c) yellow" << endl;
+    cout << "Twoja odpowiedz: ";
+    cin >> odp;
+    if (odp == "a")
+    {
+        cout << "Poprawna odpowiedz!";
+        pkt++;
+    }
+    else cout << "Zle! Petla jest polecenie for";
 
+    Sleep(3000);
+    system("cls");
+
+    cout << "Jakie zwierze zywi sie glownie mlekiem?" << endl;
+    cout << "a) pies" << endl;
+    cout << "b) krowa" << endl;
+    cout << "c) zadne z podanych" << endl;
+    cout << "Twoja odpowiedz: ";
+    cin >> odp;
+    if (odp == "c")
+    {
+        cout << "Poprawna odpowiedz!";
+        pkt++;
+    }
+    else cout << "Zle! Zadne z podanych zwierzat nie zywi sie mlekiem";
+
+    Sleep(3000);
+    system("cls");
+
+    cout << "Jaki kolor ma trawa?" << endl;
+    cout << "a) zielony" << endl;
+    cout << "b) czerwony" << endl;
+    cout << "c) purpurowy" << endl;
+    cout << "Twoja odpowiedz: ";
+    cin >> odp;
+    if (odp == "a")
+    {
+        cout << "Poprawna odpowiedz!";
+        pkt++;
+    }
+    else cout << "Zle! Trawa jest zielona";
+
+    Sleep(3000);
+    system("cls");
+
+    cout << "Brawo! zdobyles: " << pkt << "/5 punktow";
 
     return 0;
 }
